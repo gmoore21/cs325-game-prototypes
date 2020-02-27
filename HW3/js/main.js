bc0de74b2e457e259318fcd3a80ffe17647c4a3e
Gage  Moore <gagemoore@MacBook-Pro-5.local>
2020-02-26 23:57:04 -0500

    HW3

--- HW3/js/main.js
+++ HW3/js/main.js
@@ -176,41 +176,6 @@ class SceneB extends Phaser.Scene {
 
     }
 
-    collectCat (player, cat)
-{
-    star.disableBody(true, true);
-
-    //  Add and update the score
-    score += 1;
-    scoreText.setText('Cats Caught: ' + score);
-
-    if (cat.countActive(true) === 0)
-    {
-
-        gameOver = true;
-
-        // game over Text (You win, click to continue)
-        // win
-
-    }
-}
-
-    hitDog (player, dog)
-{
-    this.physics.pause();
-
-    player.setTint(0xff0000);
-
-    player.anims.play('turn');
-
-    gameOver = true;
-
-    // put code to go to end scene
-}
-
-
-
-
 }
 
 class SceneC extends Phaser.Scene {
@@ -258,3 +223,34 @@ var config = {
 
 var game = new Phaser.Game(config);
 
+function collectCat (player, cat)
+{
+    star.disableBody(true, true);
+
+    //  Add and update the score
+    score += 1;
+    scoreText.setText('Cats Caught: ' + score);
+
+    if (cat.countActive(true) === 0)
+    {
+
+        gameOver = true;
+
+        // game over Text (You win, click to continue)
+        // win
+
+    }
+}
+
+function hitDog (player, dog)
+{
+    this.physics.pause();
+
+    player.setTint(0xff0000);
+
+    player.anims.play('turn');
+
+    gameOver = true;
+
+    // put code to go to end scene
+}
